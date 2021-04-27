1e09442e2efd0b8968be534f56987d42eb0a49c0
Tarak Shah <tarak@hrdag.org>
2021-04-26 17:07:04 -0700

    create document ids after page classification

--- minutes/classify-pages/export/src/export.R
+++ minutes/classify-pages/export/src/export.R
@@ -8,7 +8,10 @@
 pacman::p_load(
     argparse,
     arrow,
-    dplyr
+    digest,
+    dplyr,
+    purrr,
+    stringr
 )
 
 parser <- ArgumentParser()
@@ -17,13 +20,32 @@ parser$add_argument("--pagetypes")
 parser$add_argument("--output")
 args <- parser$parse_args()
 
+pad_num <- function(num) str_pad(num, width=4, side="left", pad="0")
+
 mins <- read_parquet(args$minutes)
 labs <- read_parquet(args$pagetypes)
 
+docids <- labs %>%
+    arrange(fileid, pageno) %>%
+    group_by(fileid) %>%
+    mutate(newdoc = pageno == 1 | pagetype != "continue") %>%
+    ungroup %>%
+    mutate(docseqid = cumsum(newdoc)) %>%
+    group_by(fileid, docseqid) %>%
+    mutate(hashargs = paste0(fileid, ".", pad_num(pageno), collapse=".")) %>%
+    ungroup %>%
+    mutate(docid = map_chr(hashargs, digest, algo="sha1", serialize=FALSE),
+           docid = str_sub(docid, 1, 8)) %>%
+    mutate(doctype = if_else(pagetype == "continue", NA_character_, pagetype),
+           doctype = max(doctype, na.rm=TRUE)) %>%
+    select(fileid, pageno, docid, doctype)
+
 out <- mins %>%
     select(fileid, starts_with("f_"), pageno, text) %>%
-    inner_join(labs, by=c("fileid", "pageno"))
+    inner_join(docids, by=c("fileid", "pageno"))
 
 stopifnot(nrow(out) == nrow(mins))
 
 write_parquet(out, args$output)
+
+# done.
