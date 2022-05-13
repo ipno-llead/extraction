dd69a7dae2d10acfa85dc823e79d8a72f2b327fc
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2022-05-13 17:15:15 +0200

    updated language and classifier models to include finetuned training

--- news-classification/model/src/model.py
+++ news-classification/model/src/model.py
@@ -13,8 +13,8 @@ def get_args():
 
 def get_dls_lm(df):
     df = pd.read_parquet(args.lm_input)
-    dls = TextDataLoaders.from_df(df, valid_pct=0.1, text_col="content", is_lm=True, num_workers=0)
-    return dls
+    dls_lm = TextDataLoaders.from_df(df, valid_pct=0.1, text_col="content", is_lm=True, num_workers=0)
+    return dls_lm
 
 
 def train_lm(dls_lm):
@@ -23,15 +23,13 @@ def train_lm(dls_lm):
     return learn_lm
 
 
-def set_lm_lr(bs, lr):
-    lr *= bs / 128
-    return lr
-
-
 def save_lm():
     learn_lm.save("../output/learnlm_ftuned")
     learn_lm.save_encoder("../output/learnlm_ftuned_enc")
     return learn_lm
+
+
+def set_lm_lr(lr): return lr
 
 
 def get_dls_cm(args):
@@ -52,11 +50,12 @@ def get_dls_cm(args):
 
 
 def train_cm(dls_cm):
-    model = text_classifier_learner(
-        dls_cm, AWD_LSTM, drop_mult=0.5, metrics=accuracy_multi
-    )
+    model = text_classifier_learner(dls_cm, AWD_LSTM, drop_mult=0.5, metrics=accuracy)
     return model
     # metrics=[accuracy_multi, RocAucMulti(), accuracy])
+
+
+def set_cm_lr(lr): return lr
 
 
 if __name__ == "__main__":
@@ -65,26 +64,32 @@ if __name__ == "__main__":
     ##### language model #####
     dls_lm = get_dls_lm(args)
     learn_lm = train_lm(dls_lm)
+
     # lm_lr = learn_lm.lr_find()
 
     print("fitting language model")
-    # learn_lm.freeze()
-    # learn_lm.fine_tune(1, lm_lr)
-    # learn_lm.unfreeze()
-    # learn_lm.fine_tune(1, lm_lr)
+    lr = set_lm_lr(lr=1e-2)
+    learn_lm.fit_one_cycle(2, lr)  # 2 epochs
+    learn_lm.unfreeze()
+    learn_lm.fit_one_cycle(8, lr)  # 2 epochs
     learn_lm = save_lm()
 
     ##### classifier model #####
     dls_cm = get_dls_cm(args)
     learn_cm = train_cm(dls_cm)
-    learn_cm.load_encoder("learnlm_ftuned_enc")
+    # learn_cm.load_encoder("../output/learnlm_finetuned_enc")
+
     # cm_lr = learn_cm.lr_find()
 
     print("fitting classifier model")
-    # learn_cm.freeze()
-    # learn_cm.fine_tune(1, 1e-2)
-    # learn_cm.unfreeze()
-    # learn_cm.fine_tune(1, slice(cm_lr/(2.6**4),cm_lr)
+    lr = set_cm_lr(2e-2)
+    learn_cm.fit_one_cycle(1, lr) # 1 epoch
+    learn_cm.freeze_to(-2)
+    learn_cm.fit_one_cycle(3, slice(lr / (2.6 ** 4), lr)) # 3 epochs
+    learn_cm.freeze_to(-3)
+    learn_cm.fit_one_cycle(5, slice(lr / 2 / (2.6 ** 4), lr / 2)) # 5 epochs 
+    learn_cm.unfreeze()
+    learn_cm.fit_one_cycle(10, slice(lr / 10 / (2.6 ** 4), lr / 10)) # 10 epochs
 
     ##### export classifier model #####
     print("exporting model")
