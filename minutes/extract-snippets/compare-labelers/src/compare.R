e5e3a50d37591293191f75210d3dc7623f6e5fb0
Tarak Shah <tarak@hrdag.org>
2022-06-14 09:55:40 -0700

    comparing labels from different labelers (snippets)

--- minutes/extract-snippets/compare-labelers/src/compare.R
+++ minutes/extract-snippets/compare-labelers/src/compare.R
@@ -17,42 +17,111 @@ parser$add_argument("--output")
 args <- parser$parse_args()
 # }}}
 
-labs <- read_parquet(args$input) %>% filter(!is.na(docid))
+library(arrow)
+library(tidyverse)
+library(fs)
 
-xtra_id_fields <- c("fileid", "doc_pg_from", "doc_pg_to", "hrgloc")
-
-all_labs <- labs %>%
-    select(hrgno,
-           docid,
-           all_of(xtra_id_fields),
-           labeler,
-           start = labstart,
-           end = labend,
-           label) %>% distinct %>%
-    arrange(labeler, docid, hrgno,
-            fileid, doc_pg_from, doc_pg_to, hrgloc,
-            start, label) %>%
-    group_by(labeler, docid, hrgno,
-            fileid, doc_pg_from, doc_pg_to, hrgloc) %>%
-    mutate(label_seqid = seq_along(label)) %>%
-    ungroup
-
-all_labs %>%
-    distinct(docid, hrgno, labeler) %>%
-    inner_join(distinct(all_labs, docid, hrgno, labeler),
-               by = c("docid", "hrgno")) %>%
+labs <- dir_ls("../import/output/training") %>%
+    set_names(path_file) %>% set_names(path_ext_remove) %>%
+    map(read_parquet) %>%
+    map_dfr(distinct, labeler, fileid, text, start, end, label)
 
 
+    # 
+    # all_labs <- labs %>%
+    #     select(hrgno,
+    #            docid,
+    #            all_of(xtra_id_fields),
+    #            labeler,
+    #            start = labstart,
+    #            end = labend,
+    #            label) %>% distinct %>%
+    #     arrange(labeler, docid, hrgno,
+    #             fileid, doc_pg_from, doc_pg_to, hrgloc,
+    #             start, label) %>%
+    #     group_by(labeler, docid, hrgno,
+    #             fileid, doc_pg_from, doc_pg_to, hrgloc) %>%
+    #     mutate(label_seqid = seq_along(label)) %>%
+    #     ungroup
+    # 
+    # all_labs %>%
+    #     distinct(docid, hrgno, labeler) %>%
+    #     inner_join(distinct(all_labs, docid, hrgno, labeler),
+    #                by = c("docid", "hrgno")) %>%
 
-all_overlaps <- all_labs %>%
-    full_join(all_labs, by = c("docid", "hrgno"),
+
+
+overlap_setup <- labs %>%
+    full_join(labs, by = c("fileid", "text"),
               suffix = c("1", "2")) %>%
-    filter(labeler1 != labeler2) %>%
+    filter(labeler1 < labeler2) %>%
     mutate(start_off = abs(start2 - start1),
            overlap = pmax(0, pmin(end1, end2) - pmax(start1, start2)),
            max_overlap = pmax(end2 - start2, end1 - start1),
-           overlap_pct = overlap / max(overlap)) %>%
+           overlap_pct = overlap / max_overlap) %>%
+    mutate(label_agree = label1 == label2) %>%
+    select(-start_off, -overlap, -max_overlap,
+           -start1, -end1, -start2, -end2)
+
+# someone tagged the same category, but in a different location
+overlap_setup %>%
+    filter(label_agree & overlap_pct <= .01) %>%
+    distinct(fileid, text, label1) %>%
+    pivot_longer(cols = c(label1)) %>%
+    group_by(label = value)
+
+# tagged the same location, was it the same tag?
+overlap_setup %>%
     filter(overlap_pct > 0) %>%
+    group_by(fileid, text, label1) %>%
+    mutate(label_agree = if_else(any(label_agree), "agree", "disagree")) %>%
+    ungroup %>%
+    count(label1, label_agree) %>%
+    pivot_wider(names_from = label_agree, values_from = n) %>%
+    mutate(pct = agree/(agree+disagree)) %>%
+    arrange(desc(agree + disagree)) %>% print(n=Inf)
+
+    group_by(label1) %>% filter(sum(n) > 20) %>% ungroup %>%
+    filter(label2 %in% label1) %>%
+    pivot_wider(names_from = label2, values_from =n)
+
+    mutate(overlapping = overlap_pct > .1) %>%
+    group_by(fileid, text, label1) %>%
+    summarise(any_labeled = any(label_agree),
+              overlapping_agree = any(overlapping & label_agree)) %>%
+    #     filter(label1 == "accused_officer_name", !any_labeled)
+    ungroup %>%
+    #     mutate()
+    count(label1, any_labeled, overlapping_agree)
+
+
+overlap_setup %>%
+    mutate(overlapping = overlap_pct > .1) %>%
+    filter(overlapping | label_agree)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+overlaps %>%
+    mutate(label_agree = if_else(label_agree, "agree", "disagree")) %>%
+    group_by
+    count(label1, label_agree) %>%
+    pivot_wider(names_from = label_agree, values_from = n) %>%
+    mutate(agree_pct = agree/(agree+disagree))
+
+
     group_by(docid, hrgno, labeler1, labeler2, label_seqid1) %>%
     filter(overlap_pct == max(overlap_pct)) %>%
     ungroup
