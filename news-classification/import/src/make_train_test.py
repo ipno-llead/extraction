edabc829bd3ec2206158f4fedb6eccbb170f0ac4
baileyb0t <bailey@hrdag.org>
2022-08-03 09:28:14 -0700

    progress on splitting import, make-train-test

---
+++ news-classification/import/src/make_train_test.py
@@ -0,0 +1,17 @@
+# Authors:     BP
+# Maintainers: BP
+# Copyright:   2022, HRDAG, GPL v2 or later
+# =========================================
+# extraction/news-classification/import/Makefile
+
+# dependencies
+
+
+# support methods
+
+
+# main
+if __name__ == '__main__':
+    
+
+# done.
